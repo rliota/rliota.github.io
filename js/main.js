62b118aea44524225a1d4c0eacc3ffb443e62f58
rob <robertliota.public@gmail.com>
2014-08-24 19:56:57 -0400

    Virtual Section Pages implemented with nifty router parser

---
+++ js/main.js
@@ -0,0 +1,84 @@
+//DOM utils
+
+function removeClass(cls, node){
+    var replacer = new RegExp(cls, 'g');
+    var oldClassName = node.className;
+    node.className = oldClassName ? oldClassName.replace(replacer, "") : null;
+}
+
+function addClass(cls, node){
+    removeClass(cls, node);
+    node.className = [node.className, cls].join(" ");
+}
+
+
+
+
+// Routing
+var LOCAL_URL_PATTERN = "#";
+
+function onHashChange(event){
+    var newURL = event.newURL;
+    parseURL(newURL);
+}
+
+function parseURL(url){
+    var localChange = url.split(LOCAL_URL_PATTERN);
+    var targetURI = "";
+    if(localChange.length > 1){
+        localChange = localChange[1];
+        var uris = localChange.split('/');
+        var uriLen = uris.length;
+        if(uriLen > 0){
+            targetURI = uris[uriLen-1];
+        }
+    }
+    setNewActiveSection(targetURI);
+}
+
+
+
+// Routing use case
+var DEFAULT_SECTION = "about";
+function setNewActiveSection(sectionId){
+    if(sectionId === ""){
+        sectionId = DEFAULT_SECTION;
+    }
+
+    var sections = document.querySelectorAll("section[id]");
+    for(var s in sections){
+        if(sections.hasOwnProperty(s)){
+            removeClass("active", sections[s]);
+        }
+    }
+    var anchors = document.querySelectorAll("nav [href]");
+    for(var a in anchors){
+        if(anchors.hasOwnProperty(a)){
+            removeClass("active", anchors[a]);
+        }
+    }
+
+
+    var links = document.querySelectorAll('[href]');
+    for(var l in links){
+        if(links.hasOwnProperty(l)){
+            var link = links[l];
+            if(link.href && link.href.indexOf(sectionId) > -1){
+                addClass("active", link);
+            }
+        }
+    }
+
+    var section = document.getElementById(sectionId);
+    addClass("active", section);
+}
+
+
+
+//"Boot"
+window.onload = function(){
+
+    window.addEventListener("hashchange", onHashChange);
+    parseURL(window.location.href);
+
+};
